f97c662d3350079a98ea3063743b811980c716fa
bork <bork.andrew.ryan@gmail.com>
2021-09-05 14:11:20 -0700

    Created pca9685.h and .cpp

---
+++ include/pca9685.h
@@ -0,0 +1,105 @@
+#ifndef PCA9685_H
+#define PCA9685_H
+
+
+#define PCA9685_ADDRESS 0b1110000
+
+
+// REGISTERS
+
+#define MODE_1      0x00
+#define MODE_2      0x01
+#define PRESCALE    0xFE
+
+#define ALL_PWM_ON_L    0xFA
+#define ALL_PWM_ON_H    0xFB
+#define ALL_PWM_OFF_L   0xFC
+#define ALL_PWM_OFF_H   0xFD
+
+#define PWM0_ON_L   0x06
+#define PWM0_ON_H   0x07
+#define PWM0_OFF_L  0x08
+#define PWM0_OFF_H  0x09
+#define PWM1_ON_L   0x0A
+#define PWM1_ON_H   0x0B
+#define PWM1_OFF_L  0x0C
+#define PWM1_OFF_H  0x0D
+#define PWM2_ON_L   0x0E
+#define PWM2_ON_H   0x0F
+#define PWM2_OFF_H  0x10
+#define PWM2_OFF_L  0x11
+#define PWM3_ON_L   0x12
+#define PWM3_ON_H   0x13
+#define PWM3_OFF_L  0x14
+#define PWM3_OFF_H  0x15
+#define PWM4_ON_L   0x16
+#define PWM4_ON_H   0x17
+#define PWM4_OFF_L  0x18
+#define PWM4_OFF_H  0x19
+#define PWM5_ON_L   0x1A
+#define PWM5_ON_H   0x1B
+#define PWM5_OFF_L  0x1C
+#define PWM5_OFF_H  0x1D
+#define PWM6_ON_L   0x1E
+#define PWM6_ON_H   0x1F
+#define PWM6_OFF_L  0x20
+#define PWM6_OFF_H  0x21
+#define PWM7_ON_L   0x22
+#define PWM7_ON_H   0x23
+#define PWM7_OFF_L  0x24
+#define PWM7_OFF_H  0x25
+#define PWM8_ON_L   0x26
+#define PWM8_ON_H   0x27
+#define PWM8_OFF_L  0x28
+#define PWM8_OFF_H  0x29
+#define PWM9_ON_L   0x2A
+#define PWM9_ON_H   0x2B
+#define PWM9_OFF_L  0x2C
+#define PWM9_OFF_H  0x2D
+#define PWM10_ON_L  0x2E
+#define PWM10_ON_H  0x2F
+#define PWM10_OFF_L 0x30
+#define PWM10_OFF_H 0x31
+#define PWM11_ON_L  0x32
+#define PWM11_ON_H  0x33
+#define PWM11_OFF_L 0x34
+#define PWM11_OFF_H 0x35
+#define PWM12_ON_L  0x36
+#define PWM12_ON_H  0x37
+#define PWM12_OFF_L 0x38
+#define PWM12_OFF_H 0x39
+#define PWM13_ON_L  0x3A
+#define PWM13_ON_H  0x3B
+#define PWM13_OFF_L 0x3C
+#define PWM13_OFF_H 0x3D
+#define PWM14_ON_L  0x3E
+#define PWM14_ON_H  0x3F
+#define PWM14_OFF_L 0x40
+#define PWM14_OFF_H 0x41
+#define PWM15_ON_L  0x42
+#define PWM15_ON_H  0x43
+#define PWM15_OFF_L 0x44
+#define PWM15_OFF_H 0x45
+
+
+
+namespace pca9685 {
+    void open();
+    void close();
+
+    void wake_up();
+    void restart();
+
+    void set_frequency(int rate);
+
+    void set_pwm_on(int pwm, int on);
+    void set_pwm_off(int pwm, int off);
+
+    void set_pwm_percent(int pwm, double percent);
+    void set_pwm_ms(int pwm, int ms);
+
+    inline int query_reg(int reg);
+    inline void write_reg(int reg, int val);
+};
+
+#endif
---
+++ src/pca9685.cpp
@@ -0,0 +1,107 @@
+#include <pca9685.h>
+
+extern "C" {
+	#include <linux/i2c-dev.h>
+	#include <i2c/smbus.h>
+}
+
+#include <iostream>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <cstdlib>
+#include <math.h>
+
+#define read(r) i2c_smbus_read_byte_data(fd, r);
+#define write(r,v) i2c_smbus_write_byte_data(fd, r, v);
+
+int fd = -1;
+
+int pwm_regs[16][4] = {
+    {0x06, 0x07, 0x08, 0x09},
+    {0x0A, 0x0B, 0x0C, 0x0D},
+    {0x0E, 0x0F, 0x10, 0x11},
+    {0x02, 0x03, 0x14, 0x15},
+    {0x16, 0x17, 0x18, 0x19},
+    {0x1A, 0x1B, 0x1C, 0x1D},
+    {0x1E, 0x1F, 0x20, 0x21},
+    {0x22, 0x23, 0x24, 0x25},
+    {0x26, 0x27, 0x28, 0x29},
+    {0x2A, 0x2B, 0x2C, 0x2D},
+    {0x2E, 0x2F, 0x30, 0x31},
+    {0x32, 0x33, 0x34, 0x35},
+    {0x36, 0x37, 0x38, 0x39},
+    {0x3A, 0x3B, 0x3C, 0x3D},
+    {0x3E, 0x3F, 0x40, 0x41},
+    {0x42, 0x43, 0x04, 0x05},
+};
+
+
+
+void pca9685::open(){
+    fd = open("/dev/i2c-1", O_RDWR); //Open the I2C device file
+	if (fd < 0) { //Catch errors
+		std::cout << "ERR (pca9685.cpp:open()): Failed to open /dev/i2c-1. Please check that I2C is enabled with raspi-config\n"; //Print error message
+	}
+
+	int status = ioctl(fd, I2C_SLAVE,BMP390_ADDR); //Set the I2C bus to use the correct address
+	if (status < 0) {
+		std::cout << "ERR (pca9685.cpp:open()): Could not get I2C bus with " << PCA9685_ADDRESS << " address. Please confirm that this address is correct\n"; //Print error message
+	}
+}
+
+void pca9685::close(){
+    close(fd);
+}
+
+inline int pca9685::query_reg(int reg){
+    return i2c_smbus_read_byte_data(fd, reg);
+}
+
+inline void pca9685::write_reg(int reg, int val){
+    i2c_smbus_write_byte_data(fd, reg, val);
+}
+
+inline int round(double d){
+    if(d < 0){
+        return (((-d) - ((int)-d)) < 0.5 ? ((int) d) : ((int) d - 1))
+    }
+    return (d - ((int) d) < 0.5 ? (int) d : (int) d + 1);
+}
+
+int freq = 200; // hz
+int per =  1000 / freq; // 5 ms
+void pca9685::set_frequency(int frq){
+// PRESCALE_VAL -> round(25000000 / (4096 * update_rate)) - 1
+    freq = frq;
+    per = 1000 / frq;
+    int reg_val = round(25000000.0 / (4096 * freq)) - 1;
+
+    write(PRESCALE, reg_val & 0xFF);
+}
+void pca9685::set_pwm_ms(int pwm, int ms){
+    int cycle = 4096 * ms / per;
+    write(pwm_regs[pwm][0], 0);
+    write(pwm_regs[pwm][1], 0);
+    write(pwm_regs[pwm][2], (cycle & 0xF00) >> 8);
+    write(pwm_regs[pwm][3], cycle & 0xFF);
+}
+void pca9685::set_pwm_percent(int pwm, double percent){
+    
+}
+void pca9685::set_pwm_on(int pwm, int on){
+    
+}
+void pca9685::set_pwm_off(int pwm, int off){
+    
+}
+
+void pca9685::wake_up(){
+    write(MODE_1, 0b00010000);
+}
+
+void pca9685::restart(){
+
+}
