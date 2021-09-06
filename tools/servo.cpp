5313167048218635b254bb97de824229152424dd
bork <bork.andrew.ryan@gmail.com>
2021-09-06 16:57:12 -0700

    Register debug statements

--- tools/servo.cpp
+++ tools/servo.cpp
@@ -9,10 +9,13 @@ int main(){
 
     printf("Mode 1 reg: %d\n", pca9685::query_reg(MODE_1) & 0xFF);
     pca9685::set_frequency(50);
-    printf("PRESCALE register is: %d\n", pca9685::query_reg(MODE_1) & 0xFF);
+    printf("PRESCALE register is: %d\n", pca9685::query_reg(PRESCALE) & 0xFF);
     // pca9685::set_pwm_off(15, 205);
     // pca9685::set_pwm_on(15, 0);
     pca9685::set_pwm_ms(15, 1);
+    usleep(500);
+    printf("OFF registers are: %3d %3d\n", pca9685::query_reg(PWM15_OFF_H) & 0xFF, pca9685::query_reg(PWM15_OFF_L) & 0xFF);
+    printf("ON registers are: %3d %3d\n", pca9685::query_reg(PWM15_ON_H) & 0xFF, pca9685::query_reg(PWM15_ON_L) & 0xFF);
 
     usleep(5000000);
 
