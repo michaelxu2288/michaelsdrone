b8e2445546855e9462a55919586537b3341ddd14
bork <bork.andrew.ryan@gmail.com>
2021-09-06 14:52:07 -0700

    test

--- tools/servo.cpp
+++ tools/servo.cpp
@@ -9,8 +9,9 @@ int main(){
 
     printf("Mode 1 reg: %d\n", pca9685::query_reg(MODE_1) & 0xFF);
     pca9685::set_frequency(50);
-    pca9685::set_pwm_off(15, 205);
-    pca9685::set_pwm_on(15, 0);
+    // pca9685::set_pwm_off(15, 205);
+    // pca9685::set_pwm_on(15, 0);
+    pca9685::set_pwm_ms(15, 1);
 
     usleep(5000000);
 
