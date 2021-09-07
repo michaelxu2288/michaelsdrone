a28137e02ae196cb65e26ab3989a3394c379fa16
bork <bork.andrew.ryan@gmail.com>
2021-09-06 17:08:54 -0700

    moved set_frequency before wake_up

--- tools/servo.cpp
+++ tools/servo.cpp
@@ -4,12 +4,12 @@
 
 int main(){
     pca9685::init();
+    pca9685::set_frequency(50);
+    printf("PRESCALE register is: %d\n", pca9685::query_reg(PRESCALE) & 0xFF);
     pca9685::wake_up();
     usleep(500);
 
     printf("Mode 1 reg: %d\n", pca9685::query_reg(MODE_1) & 0xFF);
-    pca9685::set_frequency(50);
-    printf("PRESCALE register is: %d\n", pca9685::query_reg(PRESCALE) & 0xFF);
     // pca9685::set_pwm_off(15, 205);
     // pca9685::set_pwm_on(15, 0);
     pca9685::set_pwm_ms(15, 1);
