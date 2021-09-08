1e10e6704f258bdc0ddaada56f4cc78f5126540b
bork <bork.andrew.ryan@gmail.com>
2021-09-08 07:58:27 -0700

    Created new logger

---
+++ include/logger.h
@@ -0,0 +1,23 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+namespace logger {
+
+    enum level {
+        debug, info, critical, warn, error
+    };
+
+    void set_log_file(char * filename);
+
+    void log(level lvl, char * msg);
+
+
+    void debug(char * msg) {log(level::debug, msg)}
+    void info(char * msg) {log(level::info, msg)}
+    void crit(char * msg) {log(level::critical, msg)}
+    void warn(char * msg) {log(level::warn, msg)}
+    void err(char * msg) {log(level::error, msg)}
+
+};
+
+#endif
---
+++ src/logger.cpp
@@ -0,0 +1,41 @@
+#include <stdio>
+#include <time.h>
+#include <logger.h>
+#include <time.h>
+
+void logger::set_log_file(char * filename){
+
+}
+
+static char buf100[100];
+static char buf20A[20];
+static char buf20B[20];
+
+void logger::log(level lvl, char * msg){
+    
+    switch (lvl)
+    {
+    case level::debug:
+        buf20B = "Debug";
+        break;
+    case level::info:
+        buf20B = "Info";
+        break;
+    case level::warn:
+        buf20B = "WARNING";
+        break;
+    case level::critical:
+        buf20B = "CRITICAL";
+        break;
+    case level::error:
+        buf20B = "ERROR";    
+        break;
+    }
+
+    time_t rawtime;
+    time(&rawtime);
+    struct tm * timeinfo = localtime(&rawtime);
+    strftime(buf20A, "%c", timeinfo);
+    sprintf(buf100, "[%s %s] %s", buf20B, buf20A, msg)
+    puts(buf100);
+}
