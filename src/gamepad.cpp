f2a8d80d2016481325d31673056a00b46dbaf085
bork <bork.andrew.ryan@gmail.com>
2021-09-18 14:44:44 -0700

    adfs

--- src/gamepad.cpp
+++ src/gamepad.cpp
@@ -36,15 +36,15 @@ static void polling_thread_function(){
 
         ssize_t bytes = read(fd, &event, event_s);
         // printf("| Time: %10d | Event Type: %#3d | Number: %2d | Value: %8d |\n", event.time, event.type, event.number, event.value);
-        std::cout << "fuck yo\n";
+        //std::cout << "fuck yo\n";
         if(bytes > 0){
-            //printf("| Time: %10d | Event Type: %#3d | Number: %2d | Value: %8d |\n", event.time, event.type, event.number, event.value);
+            printf("| Time: %10d | Event Type: %#3d | Number: %2d | Value: %8d |\n", event.time, event.type, event.number, event.value);
         
-            // if((event.type & JS_EVENT_AXIS) == JS_EVENT_AXIS){
-            //     axises[event.number] = ((double)(event.value)) / 32767.0;
-            // }else if((event.type & JS_EVENT_BUTTON) ==JS_EVENT_BUTTON){
-            //     buttons[event.number] = event.value;
-            // }
+            if((event.type & JS_EVENT_AXIS) == JS_EVENT_AXIS){
+                axises[event.number] = ((double)(event.value)) / 32767.0;
+            }else if((event.type & JS_EVENT_BUTTON) ==JS_EVENT_BUTTON){
+                buttons[event.number] = event.value;
+            }
         }
 
         usleep(10000);
