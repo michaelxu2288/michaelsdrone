6365466936cebb035a95753b0a962c48408f4ac6
bork <bork.andrew.ryan@gmail.com>
2022-06-29 16:47:56 -0700

    fdsa

--- tools/gamepad_test.cpp
+++ tools/gamepad_test.cpp
@@ -6,7 +6,7 @@ int main(){
     gamepad::init();
 
     while(true){
-        printf("Axis 0: %1.3f | Axis 1: %1.3f | Axis 2: %1.3f | Axis 3: %1.3f | Axis 4: %1.3f | Axis 5: %1.3f\n", gamepad::get_axis(0), gamepad::get_axis(1), gamepad::get_axis(2), gamepad::get_axis(3), gamepad::get_axis(4), gamepad::get_axis(5));
+        printf("Axis 0: %5.3f | Axis 1: %5.3f | Axis 2: %5.3f | Axis 3: %5.3f | Axis 4: %5.3f | Axis 5: %5.3f\n", gamepad::get_axis(0), gamepad::get_axis(1), gamepad::get_axis(2), gamepad::get_axis(3), gamepad::get_axis(4), gamepad::get_axis(5));
         usleep(100000);
     }
     gamepad::stop();
