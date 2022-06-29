983f5c94f90b16999ee986664bac8becaddd9daf
bork <bork.andrew.ryan@gmail.com>
2022-06-29 01:44:43 -0700

    fdasfdas

--- tools/accl_test.cpp
+++ tools/accl_test.cpp
@@ -3,8 +3,10 @@
 #include <iostream>
 
 #include <socket.h>
+#include <logger.h>
 
 int main(){
+    logger::debug("Initializing mpu6050.");
     mpu6050::init();
     mpu6050::set_accl_set(mpu6050::accl_range::g_16);
     mpu6050::set_gyro_set(mpu6050::gyro_range::deg_1000);
@@ -12,6 +14,7 @@ int main(){
     mpu6050::set_fsync(mpu6050::fsync::input_dis);
     mpu6050::set_dlpf_bandwidth(mpu6050::dlpf::hz_184);
     mpu6050::wake_up();
+    logger::debug("Finished initializing mpu6050.");
     // setvbuf(stdout, NULL,_IONBF,0);
     
 
@@ -19,9 +22,10 @@ int main(){
     float x,y,z;
     double data[6];
 
+    logger::debug("Setting up sensor loop.");
     while(1){
         mpu6050::read(data);
-
+        logger::debug("in loop");
         // std::cout<< data[0] << " " << data[1] << " " << data[2] << " " << data[3] << " " << data[4] << " " << data[5] << "\n";
         usleep(1000);
     }
