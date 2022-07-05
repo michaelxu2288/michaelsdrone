4baa79bc5d51ef4d4040e321e7a51f9a0cd65938
bork <bork.andrew.ryan@gmail.com>
2022-07-04 20:22:09 -0700

    fdas

--- src/drone.cpp
+++ src/drone.cpp
@@ -295,7 +295,7 @@ void drone::init_sensors(bool thread) {
     mpu6050::set_gyro_set(mpu6050::gyro_range::deg_2000);
     mpu6050::set_clk(mpu6050::clk::y_gyro);
     mpu6050::set_fsync(mpu6050::fsync::input_dis);
-    mpu6050::set_dlpf_bandwidth(mpu6050::dlpf::hz_94);
+    mpu6050::set_dlpf_bandwidth(mpu6050::dlpf::hz_5);
     mpu6050::wake_up();
 
     mpu6050::calibrate(2000);
