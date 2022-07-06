e521894694d79eabf7090e3b38238659fecd23b8
bork <bork.andrew.ryan@gmail.com>
2022-07-06 14:06:49 -0700

    fdsafds

--- src/drone.cpp
+++ src/drone.cpp
@@ -327,7 +327,7 @@ void sensor_thread_funct(){
 
             temp = velocity * dt;
             position = position + temp;
-            temp = math::vector(filtered_mpu6050_data[0]*dt*G, filtered_mpu6050_data[1]*dt*G, filtered_mpu6050_data[2]*dt*G);
+            temp = math::vector(filtered_mpu6050_data[0]*dt*G, filtered_mpu6050_data[1]*dt*G, -filtered_mpu6050_data[2]*dt*G);
             temp = math::quarternion::rotateVector(orientation, temp);
             temp.z += G;
             velocity = velocity + temp;
