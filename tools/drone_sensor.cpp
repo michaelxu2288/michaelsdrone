940604c62257cb058a4fed41229840ce73dbc8b4
bork <bork.andrew.ryan@gmail.com>
2022-07-06 15:45:30 -0700

    fdsadsf

--- tools/drone_sensor.cpp
+++ tools/drone_sensor.cpp
@@ -16,14 +16,20 @@ void ctrl_thrd(){
     double deg_t_rad = 3.141592 / 180;
     double roll = config::get_config_dbl("ctrl_max_roll", 5) * deg_t_rad;
     double pitch = config::get_config_dbl("ctrl_max_pitch", 5) * deg_t_rad;
+    double vz = config::get_config_dbl("ctrl_max_vz", 5) * deg_t_rad;
+    double vyaw = config::get_config_dbl("ctrl_max_vyaw", 5) * deg_t_rad;
     config::write_to_file();
 
     gamepad::init();
     pid * roll_ctrl = drone::get_roll_controller();
     pid * pitch_ctrl = drone::get_pitch_controller();
+    pid * vyaw_ctrl = drone::get_vyaw_controller();
+    pid * z_ctrl = drone::get_z_controller();
     while(1){
         roll_ctrl->change_setpoint(gamepad::get_axis(3) * roll);
         pitch_ctrl->change_setpoint(gamepad::get_axis(4) * pitch);
+        z_ctrl->change_setpoint(z_ctrl->setpoint + gamepad::get_axis(1) * vz);
+        vyaw_ctrl->change_setpoint(((0.5 + 0.5 * gamepad::get_axis(2)) - (0.5 + 0.5 * gamepad::get_axis(5))) * vyaw);
         usleep(10000);
     }
     gamepad::stop();
