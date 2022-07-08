7b7d7018c425ccf9a324d95ecb2c563b9c0c5a40
bork <bork.andrew.ryan@gmail.com>
2022-07-07 22:36:39 -0700

    fdas

--- server/server.js
+++ server/server.js
@@ -113,27 +113,27 @@ server.listen(port, () => {
                 running_process.kill();
             }
 
-            console.log("Running test");
-            const test = exec("sudo ./bin/drone_sensor", (err, stdout, stderr) => {
-                console.log("bruh why", err);
-            });
+            // console.log("Running test");
+            // const test = exec("sudo ./bin/drone_sensor", (err, stdout, stderr) => {
+            //     console.log("bruh why", err);
+            // });
 
-            test.kill();
+            // test.kill();
 
             // execSync(`sudo ./bin/${bruh}`);
-            // console.log("git pull: ");
-            // running_process = exec("git pull", (err, stdout, stderr) => {
-            //     console.log(stdout);
-            //     console.log(`make ${bruh}`);
-            //     running_process = exec(`make ${bruh}`, (err, stdout, stderr) => {
-            //         console.log(stdout);
-            //         console.log(`sudo ./bin/${bruh}`);
-            //         running_process = exec(`sudo ./bin/${bruh}`, (err, stdout, stderr) => {
-            //             console.log(stdout);
-            //             running_process = null;
-            //         });
-            //     });
-            // });
+            console.log("git pull: ");
+            running_process = exec("git pull", (err, stdout, stderr) => {
+                console.log(stdout);
+                console.log(`make ${bruh}`);
+                running_process = exec(`make ${bruh}`, (err, stdout, stderr) => {
+                    console.log(stdout);
+                    console.log(`sudo ./bin/${bruh}`);
+                    running_process = exec(`sudo ./bin/${bruh}`, (err, stdout, stderr) => {
+                        console.log(stdout);
+                        running_process = null;
+                    });
+                });
+            });
         }); 
     });
 
