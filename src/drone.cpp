f1de973513616a22364b79c1b1a93aaedc734cac
bork <bork.andrew.ryan@gmail.com>
2022-07-08 20:01:09 -0700

    fsad

--- src/drone.cpp
+++ src/drone.cpp
@@ -554,6 +554,7 @@ void drone::init_sensors(bool thread) {
     logger::info("Initializing BMP390.");
     bmp390::init();
     bmp390::soft_reset();
+    usleep(10000);
     bmp390::set_oversample(bmp390::STANDARD, bmp390::ULTRA_LOW_POWER);
     bmp390::set_iir_filter(bmp390::COEFF_3);
     // bmp390::set_enable(true, true);
