b1daefe431a01eaeec12975ea3731034d3761f56
bork <bork.andrew.ryan@gmail.com>
2022-07-10 01:20:37 -0700

    fafsda

--- tools/config.cpp
+++ tools/config.cpp
@@ -3,14 +3,16 @@
 #include <json.hpp>
 
 int main(){
-    // nlohmann::json s = nlohmann::json::object(); // json::Object();
-    // s["bruh"] = nlohmann::json::object();
-    // logger::info("{}", s.dump());
-    // s = s["bruh"];
-    // logger::info("{}", s.dump());
+    nlohmann::json s = nlohmann::json::object(); // json::Object();
+    nlohmann::json k = s;
+    s["bruh"] = nlohmann::json::object();
+    logger::info("{}", s.dump());
+    s = s["bruh"];
+    logger::info("{}", s.dump());
+    logger::info("{}", k.dump());
 
-    config::load_file("config/test.json");
-    logger::info("{}", config::get_config_int("a.b.c.d", 1));
-    logger::info("{}", config::get_config_dbl("a.b.c.d", 1));
-    config::write_to_file("config/test.json");
+    // config::load_file("config/test.json");
+    // logger::info("{}", config::get_config_int("a.b.c.d", 1));
+    // logger::info("{}", config::get_config_dbl("a.b.c.d", 1));
+    // config::write_to_file("config/test.json");
 }
