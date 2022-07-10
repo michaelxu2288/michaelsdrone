f2fdab95b3fe078ae7944dcabb321a9fa1f00b48
bork <bork.andrew.ryan@gmail.com>
2022-07-10 00:12:16 -0700

    fdsadafs

--- tools/config.cpp
+++ tools/config.cpp
@@ -3,12 +3,12 @@
 #include <json.hpp>
 
 int main(){
-    // json::JSON s = json::Object();
-    // s["bruh"] = json::Object();
-    // logger::info("{}", s.dump());
-    // s = s["bruh"];
-    // logger::info("{}", s.dump());
-    config::load_file("config/test.json");
-    config::get_config_int("a.b.c.d", 1);
-    config::write_to_file("config/test.json");
+    json::JSON s = json::Object();
+    s["bruh"] = json::Object();
+    logger::info("{}", s.dump());
+    s = s["bruh"];
+    logger::info("{}", s.dump());
+    // config::load_file("config/test.json");
+    // config::get_config_int("a.b.c.d", 1);
+    // config::write_to_file("config/test.json");
 }
