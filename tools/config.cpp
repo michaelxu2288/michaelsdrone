3a280f33d7e28b25db5fb845b95c6d5c0f6490f0
bork <bork.andrew.ryan@gmail.com>
2022-07-10 01:26:59 -0700

    fsadfasdf

--- tools/config.cpp
+++ tools/config.cpp
@@ -4,11 +4,11 @@
 
 int main(){
     nlohmann::json s = nlohmann::json::object(); // json::Object();
-    nlohmann::json k = s["a"];
-    k = k["b"];
-    k = k["c"];
-    k = k["d"];
-    k = 0;
+    nlohmann::json * k = &s["a"];
+    k = &((*k)["b"]);
+    k = &((*k)["c"]);
+    k = &((*k)["d"]);
+    *k = 0;
     // k["a"]["b"]["c"]["d"] = 0;
     // s["bruh"] = nlohmann::json::object();
     logger::info("{}", s.dump());
