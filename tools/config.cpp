e294f9a69fcadc5df12cd5103771a56aea2153b2
bork <bork.andrew.ryan@gmail.com>
2022-07-10 00:11:08 -0700

    fdasfdsa

--- tools/config.cpp
+++ tools/config.cpp
@@ -4,12 +4,11 @@
 
 int main(){
     // json::JSON s = json::Object();
-    // json::JSON s = json::Object();
     // s["bruh"] = json::Object();
     // logger::info("{}", s.dump());
     // s = s["bruh"];
     // logger::info("{}", s.dump());
-    // config::load_file("config/test.json");
-    // config::get_config_int("a.b.c.d", 1);
-    // config::write_to_file("config/test.json");
+    config::load_file("config/test.json");
+    config::get_config_int("a.b.c.d", 1);
+    config::write_to_file("config/test.json");
 }
