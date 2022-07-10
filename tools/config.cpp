d4d33efe8df4f580f585ef25bcc77824068da2e1
bork <bork.andrew.ryan@gmail.com>
2022-07-09 23:58:12 -0700

    fdsadsf

--- tools/config.cpp
+++ tools/config.cpp
@@ -1,8 +1,14 @@
 #include <config.h>
 #include <logger.h>
+#include <json.h>
 
 int main(){
-    config::load_file("config/test.json");
-    config::get_config_int("a.b.c.d", 1);
-    config::write_to_file("config/test.json");
+    json::JSON s = json::Object();
+    s["bruh"] = json::Object();
+    logger::info("{}", s.dump());
+    s = s["bruh"];
+    logger::info("{}", s.dump());
+    // config::load_file("config/test.json");
+    // config::get_config_int("a.b.c.d", 1);
+    // config::write_to_file("config/test.json");
 }
