8d3f17ba5918505809f2ad9d02e93e00d4d2d54b
bork <bork.andrew.ryan@gmail.com>
2022-07-11 00:34:43 -0700

    fdsasd

--- src/drone.cpp
+++ src/drone.cpp
@@ -494,7 +494,7 @@ void sensor_thread_funct(){
             temp.z += G*dt;
             velocity = velocity + temp;
             // velocity.z = vzfilter[velocity.z];
-            velocity.z = velocity.z * sensor_z_tau - valt * (1 - sensor_z_tau);
+            velocity.z = velocity.z * sensor_z_tau + valt * (1 - sensor_z_tau);
         }
 
         {// PID updates
