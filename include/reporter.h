7bbbaf6dfb04f2c4c38ed02f83261c8d706b80e5
bork <bork.andrew.ryan@gmail.com>
2022-07-16 22:43:17 -0700

    fdsafads

--- include/reporter.h
+++ include/reporter.h
@@ -1,12 +1,12 @@
 #ifndef REPORTER_H
 #define REPORTER_H
 
-
+// #include <string_view>
 
 namespace reporter {
     // void set_connection()
 
-    void bind_str(const char * name, const * str);
+    void bind_str(const char * name, char * str);
     void bind_bool(const char * name, bool * value);
     void bind_int(const char * name, int * value);
     void bind_dbl(const char * name, double * value);
--- src/reporter.cpp
+++ src/reporter.cpp
@@ -13,6 +13,8 @@ static vector<uint8_t> types;
 
 
 
-void reporter::bind_bool(){
-
+void reporter::bind_bool(const char * name, bool * value){
+    names.push_back(name);
+    bindings.push_back((void*) value);
+    types.push_back(0);
 }
