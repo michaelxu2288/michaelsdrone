04d4540471441ff78502b70964fecbd0b8532a86
bork <bork.andrew.ryan@gmail.com>
2022-07-16 22:46:18 -0700

    fadsfads

--- tools/enum_test.cpp
+++ tools/enum_test.cpp
@@ -14,4 +14,16 @@ int main(){
     logger::info("2: {}", *k);
     int * j = (int *) k;
     logger::info("3: {}", *j);
+    s = balls;
+    logger::info("1: {}", s);
+    test * k = &s;
+    logger::info("2: {}", *k);
+    int * j = (int *) k;
+    logger::info("3: {}", *j);
+    s = dick;
+    logger::info("1: {}", s);
+    test * k = &s;
+    logger::info("2: {}", *k);
+    int * j = (int *) k;
+    logger::info("3: {}", *j);
 }
