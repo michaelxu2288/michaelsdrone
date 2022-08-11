590b3d58388cb8f043eb9e4d812027d56e9a942b
bork <bork.andrew.ryan@gmail.com>
2022-08-11 02:04:19 -0700

    afdsafdsa

--- src/parameters.cpp
+++ src/parameters.cpp
@@ -31,7 +31,9 @@ void parameters::bind_bool(const char * name, bool * value, bool readonly){
         if(i != 0){
             writable_ids += ",";
         }
-        writable_ids += name + ":" + std::to_string(i);
+        writable_ids += name;
+        writable_ids += ":";
+        writable_ids += std::to_string(i);
 
         writable_names.push_back(name);
         writable_bindings.push_back((void*) value);
@@ -50,7 +52,9 @@ void parameters::bind_int(const char * name, int * value, bool readonly){
         if(i != 0){
             writable_ids += ",";
         }
-        writable_ids += name + ":" + std::to_string(i);
+        writable_ids += name;
+        writable_ids += ":";
+        writable_ids += std::to_string(i);
 
         writable_names.push_back(name);
         writable_bindings.push_back((void*) value);
@@ -69,7 +73,9 @@ void parameters::bind_dbl(const char * name, double * value, bool readonly){
         if(i != 0){
             writable_ids += ",";
         }
-        writable_ids += name + ":" + std::to_string(i);
+        writable_ids += name;
+        writable_ids += ":";
+        writable_ids += std::to_string(i);
 
         writable_names.push_back(name);
         writable_bindings.push_back((void*) value);
@@ -88,7 +94,9 @@ void parameters::bind_str(const char * name, std::string * value, bool readonly)
         if(i != 0){
             writable_ids += ",";
         }
-        writable_ids += name + ":" + std::to_string(i);
+        writable_ids += name;
+        writable_ids += ":";
+        writable_ids += std::to_string(i);
 
         writable_names.push_back(name);
         writable_bindings.push_back((void*) value);
