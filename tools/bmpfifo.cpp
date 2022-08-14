3e4e6c460ed5111e7c8dfa353f0522b0d564a685
bork <bork.andrew.ryan@gmail.com>
2022-08-14 16:20:01 -0700

    fdsafds

--- tools/bmpfifo.cpp
+++ tools/bmpfifo.cpp
@@ -9,7 +9,7 @@ int main() {
     bmp390::soft_reset();
     bmp390::set_oversample(bmp390::oversampling::HIGH, bmp390::ULTRA_LOW_POWER);
     bmp390::set_iir_filter(bmp390::COEFF_7);
-    bmp390::set_output_data_rate(bmp390::hz100);
+    bmp390::set_output_data_rate(bmp390::hz50);
     bmp390::set_enable(true, true);
 
     bmp390::set_enable_fifo(true, true);
