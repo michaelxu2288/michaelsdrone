4e38c32263d529738557eabe6531aadb19a5ccdd
bork <bork.andrew.ryan@gmail.com>
2022-08-14 16:15:58 -0700

    fdsadfas

--- tools/bmpfifo.cpp
+++ tools/bmpfifo.cpp
@@ -8,8 +8,8 @@ int main() {
     bmp390::init();
     bmp390::soft_reset();
     bmp390::set_oversample(bmp390::oversampling::HIGH, bmp390::ULTRA_LOW_POWER);
-    bmp390::set_iir_filter(bmp390::COEFF_3);
-    bmp390::set_output_data_rate(bmp390::hz50);
+    bmp390::set_iir_filter(bmp390::COEFF_7);
+    bmp390::set_output_data_rate(bmp390::hz100);
     bmp390::set_enable(true, true);
 
     bmp390::set_enable_fifo(true, true);
@@ -29,14 +29,14 @@ int main() {
     // bmp390::read_fifo(data);
     // logger::info("Temp: {:10.2f} C, Pressure: {:10.0f} P, Elevation: {:10.1f} ft", data[0], data[1], data[2] * 3.28084);
 
-    for(int i = 0; i< 10; i ++) {
+    // for(int i = 0; i< 10; i ++) {
         
-        bmp390::read_fifo(data);
-        usleep(burh);
-    }
-    double pressure = bmp390::get_press(bmp390::get_temp());
-    logger::info("Pressure benchmark {:10.0f}", pressure);
-    bmp390::set_pressure_benchmark(pressure);
+    //     bmp390::read_fifo(data);
+    //     usleep(burh);
+    // }
+    // double pressure = bmp390::get_press(bmp390::get_temp());
+    // logger::info("Pressure benchmark {:10.0f}", pressure);
+    // bmp390::set_pressure_benchmark(pressure);
 
     while(1){
         bmp390::read_fifo(data);
