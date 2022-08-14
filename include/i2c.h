99a65843d9714e421a59b5f984167c6c96c1cbdf
bork <bork.andrew.ryan@gmail.com>
2022-08-13 17:24:51 -0700

    fdsafdas

--- include/i2c.h
+++ include/i2c.h
@@ -1,13 +1,23 @@
 
 namespace i2c{
 
-    int get_device(int addr);
-    int close_device(int fd);
+    // int get_device(int addr);
+    // int close_device(int fd);
 
-    char read_byte(int fd, char reg);
-    void read_burst(int fd, char reg, char * buf, int len);
+    // char read_byte(int fd, char reg);
+    // void read_burst(int fd, char reg, char * buf, int len);
 
-    void write_byte(int fd, char reg, char val);
-    void write_burst(int fd, char reg, char * buf, int len);
+    // void write_byte(int fd, char reg, char val);
+    // void write_burst(int fd, char reg, char * buf, int len);
+    struct device {
+        int fd;
+        int addr;
 
+        device(int addr);
+        char read_byte(char reg);
+        void read_burst(char reg, char * buf, int len);
+
+        void write_byte(char reg, char val);
+        void write_burst(char reg, char * buf, int len);
+    }
 }
--- src/i2c.cpp
+++ src/i2c.cpp
@@ -11,59 +11,130 @@ extern "C" {
 	#include <i2c/smbus.h>
 }
 
-int i2c::get_device(int addr) {
-    int fd = open("/dev/i2c-1", O_RDWR); //Open the I2C device file
+
+// /* This is the structure as used in the I2C_SMBUS ioctl call */
+// struct i2c_smbus_ioctl_data {
+// 	__u8 read_write;
+// 	__u8 command;
+// 	__u32 size;
+// 	union i2c_smbus_data __user *data;
+// };
+
+// /* This is the structure as used in the I2C_RDWR ioctl call */
+// struct i2c_rdwr_ioctl_data {
+// 	struct i2c_msg __user *msgs;	/* pointers to i2c_msgs */
+// 	__u32 nmsgs;			/* number of i2c_msgs */
+// };
+
+// struct i2c_msg {
+//         __u16 addr;        /* slave address                        */
+//         __u16 flags;
+// #define I2C_M_TEN                0x0010        /* this is a ten bit chip address */
+// #define I2C_M_RD                0x0001        /* read data, from slave to master */
+// #define I2C_M_NOSTART                0x4000        /* if I2C_FUNC_PROTOCOL_MANGLING */
+// #define I2C_M_REV_DIR_ADDR        0x2000        /* if I2C_FUNC_PROTOCOL_MANGLING */
+// #define I2C_M_IGNORE_NAK        0x1000        /* if I2C_FUNC_PROTOCOL_MANGLING */
+// #define I2C_M_NO_RD_ACK                0x0800        /* if I2C_FUNC_PROTOCOL_MANGLING */
+// #define I2C_M_RECV_LEN                0x0400        /* length will be first received byte */
+//         __u16 len;                /* msg length                                */
+//         __u8 *buf;                /* pointer to msg data                        */
+// };
+
+// int i2c::get_device(int addr) {
+//     int fd = open("/dev/i2c-1", O_RDWR); //Open the I2C device file
+// 	if (fd < 0) { //Catch errors
+// 		logger::err("Failed to open /dev/i2c-1. Please check that I2C is enabled with raspi-config"); //Print error message
+// 	}
+
+// 	int status = ioctl(fd, I2C_SLAVE, addr); //Set the I2C bus to use the correct address
+// 	if (status < 0) {
+// 		logger::err("Could not get I2C bus with {} address. Please confirm that this address is correct", addr);
+// 	}
+//     return fd;
+// }
+
+// int i2c::close_device(int fd) {
+//     return close(fd);
+// }
+
+// char i2c::read_byte(int fd, char reg) {
+//     char buf = reg;
+//     int n = read(fd, &buf, 1);
+//     if(n != 1){
+//         perror("BRUH: ");
+//     }
+//     return buf;
+// }
+
+// void i2c::read_burst(int fd, char reg, char * buf, int len) {
+//     buf[0] = reg;
+//     int n = read(fd, buf, len);
+//     if(n != len){
+//         perror("COCK: ");
+//     }
+// }
+
+// void i2c::write_byte(int fd, char reg, char val) {
+//     char buf[2];
+//     buf[0] = reg;
+//     buf[1] = val;
+//     int n = write(fd, buf, 2);
+//     if(n != 1){
+//         perror("WRITE: ");
+//     }
+// }
+
+// void i2c::write_burst(int fd, char reg, char * buf, int len) {
+//     if(len > 20) {logger::err("Too many bytes to write: {}", len); return;}
+//     char writebuf[21];
+//     writebuf[0] = reg;
+//     while(len--){
+//         writebuf[len+1] = buf[len];
+//     }
+//     int n = write(fd, buf, len+1);
+//     if(n != len+1){
+//         perror("WTF: ");
+//     }
+// }
+
+
+i2c::device::device(int _addr) {
+    addr = _addr;
+    
+    fd = open("/dev/i2c-1", O_RDWR); //Open the I2C device file
 	if (fd < 0) { //Catch errors
 		logger::err("Failed to open /dev/i2c-1. Please check that I2C is enabled with raspi-config"); //Print error message
+        return;
 	}
 
 	int status = ioctl(fd, I2C_SLAVE, addr); //Set the I2C bus to use the correct address
 	if (status < 0) {
 		logger::err("Could not get I2C bus with {} address. Please confirm that this address is correct", addr);
 	}
-    return fd;
 }
 
-int i2c::close_device(int fd) {
-    return close(fd);
-}
-
-char i2c::read_byte(int fd, char reg) {
-    char buf = reg;
-    int n = read(fd, &buf, 1);
-    if(n != 1){
-        perror("BRUH: ");
-    }
-    return buf;
-}
-
-void i2c::read_burst(int fd, char reg, char * buf, int len) {
+char i2c::device::read_byte(char reg) {
+    i2c_rdwr_ioctl_data data;
+    i2c_msg msgs[2];
+    char buf[1];
     buf[0] = reg;
-    int n = read(fd, buf, len);
-    if(n != len){
-        perror("COCK: ");
-    }
-}
+    msgs[0].addr = addr;
+    msgs[0].buf = buf;
+    msgs[0].flag = 0;
+    msgs[0].len = 1;
+    
+    msgs[1].addr = addr;
+    msgs[1].buf = buf;
+    msgs[1].flag = I2C_M_READ;
+    msgs[1].len = 1;
 
-void i2c::write_byte(int fd, char reg, char val) {
-    char buf[2];
-    buf[0] = reg;
-    buf[1] = val;
-    int n = write(fd, buf, 2);
-    if(n != 1){
-        perror("WRITE: ");
-    }
-}
+    data.msgs = msgs;
+    data.nmsgs = 2;
 
-void i2c::write_burst(int fd, char reg, char * buf, int len) {
-    if(len > 20) {logger::err("Too many bytes to write: {}", len); return;}
-    char writebuf[21];
-    writebuf[0] = reg;
-    while(len--){
-        writebuf[len+1] = buf[len];
+    int n = ioctl(fd, I2C_RDWR, &data);
+    if(n < 0) {
+        perror("Error writing byte");
     }
-    int n = write(fd, buf, len+1);
-    if(n != len+1){
-        perror("WTF: ");
-    }
+
+    return buf[0];
 }
