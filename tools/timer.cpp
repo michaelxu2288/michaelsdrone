a7fc1e6d8f09a6efaf4a0eab31fd21b5244252cc
bork <bork.andrew.ryan@gmail.com>
2022-08-14 23:46:35 -0700

    fdsadfas

--- tools/timer.cpp
+++ tools/timer.cpp
@@ -12,13 +12,14 @@ auto now = std::chrono::steady_clock::now();
 void test() {
     double dt = std::chrono::duration_cast<std::chrono::nanoseconds> (now - then).count() * 0.000000001;
     int t_since = std::chrono::duration_cast<std::chrono::nanoseconds> (now - start).count();
+    usleep(20000);
     logger::info("i: {:10d} {:10f} ", i, dt);
     i++;
 }
 
 int main() {
-    timer t(test, 1000);
+    timer t(test, 31415);
     while(true) {
-        usleep(31415);
+        usleep(10000);
     }
 }
