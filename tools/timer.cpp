598bb8937b829f13a8d0fcda049d46f70828b669
bork <bork.andrew.ryan@gmail.com>
2022-08-15 00:05:45 -0700

    fdasfdsa

--- tools/timer.cpp
+++ tools/timer.cpp
@@ -15,7 +15,7 @@ void test() {
     now = std::chrono::steady_clock::now();
     double dt = std::chrono::duration_cast<std::chrono::nanoseconds> (now - then).count() * 0.000000001;
     then = now;
-    int s = 10000 * sin(i) + 10000;
+    int s = 20000 * sin(i) + 15000;
     usleep(s);
     logger::info("i: {:10d} sleep: {:10f} s freq: {:10.1f} hz ", i, s/1000000000.0, 1/dt);
     i++;
