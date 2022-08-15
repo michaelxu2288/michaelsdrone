0c0a4daa67e66c656f8e5a5c6049ec4d10ab970b
bork <bork.andrew.ryan@gmail.com>
2022-08-14 21:27:31 -0700

    fdsadsf

--- src/timer.cpp
+++ src/timer.cpp
@@ -4,18 +4,26 @@
 timer::timer() {
     running = false;
 }
-
+static void bruh(std::function<void(void)> & command, unsigned int & interval, std::mutex & m){
+    auto chrono_interval = std::chrono::milliseconds(interval);
+    while(running) {
+        std::lock_guard <std::mutex> lock(thread_mutex);
+        auto next = std::chrono::steady_clock::now() + chrono_interval;
+        command();
+        std::this_thread::sleep_until(next);
+    }
+}
 timer::timer(std::function<void(void)> _command, unsigned int _interval_ms) {
     running = true;
     command = _command;
     interval = _interval_ms;
-    thread = std::thread((*this));
+    thread = std::thread(bruh, command, interval, thread_mutex);
 }
 
 void timer::operator() (){
     auto chrono_interval = std::chrono::milliseconds(interval);
     while(running) {
-        // std::lock_guard <std::mutex> lock(thread_mutex);
+        std::lock_guard <std::mutex> lock(thread_mutex);
         auto next = std::chrono::steady_clock::now() + chrono_interval;
         command();
         std::this_thread::sleep_until(next);
