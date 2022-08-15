4bc4e4718f6e86084af81479cc052fefa24a88e1
bork <bork.andrew.ryan@gmail.com>
2022-08-14 21:33:53 -0700

    fdsafds

--- src/timer.cpp
+++ src/timer.cpp
@@ -4,7 +4,8 @@
 timer::timer() {
     running = false;
 }
-static void bruh(std::function<void(void)> & command, bool & running, unsigned int & interval, std::mutex & m){
+
+static void bruh(std::function<void(void)> command, bool running, unsigned int interval){
     auto chrono_interval = std::chrono::milliseconds(interval);
     while(running) {
         std::lock_guard <std::mutex> lock(m);
@@ -13,6 +14,7 @@ static void bruh(std::function<void(void)> & command, bool & running, unsigned i
         std::this_thread::sleep_until(next);
     }
 }
+
 timer::timer(std::function<void(void)> _command, unsigned int _interval_ms) {
     running = true;
     command = _command;
