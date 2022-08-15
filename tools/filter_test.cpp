866c65f472f264e7a2e498d183711aa1f0a5991d
bork <bork.andrew.ryan@gmail.com>
2022-08-15 14:57:49 -0700

    fads

--- tools/filter_test.cpp
+++ tools/filter_test.cpp
@@ -9,16 +9,20 @@
 timer t;
 filter::filter low_pass = filter::low_pass(24, 5);
 double out = 0;
+
+
 void loop() {
     out += t.dt;
 }
+
+
 
 int main() {
     t.start(loop, 1000 / 24);
     parameters::bind_dbl("out", &out, true);
 
     sock::socket client(sock::unix, sock::tcp);
-    sock::un_connection unix_connection = client.un_connect(socket_path.c_str());
+    sock::un_connection unix_connection = client.un_connect("/home/pi/drone/run/drone");
 
     if(!unix_connection.valid){
         // reconnect_node_server(client, unix_connection);
