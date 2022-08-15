0279891ac06c0a9e0398b8a960e0ae2f5e16792e
bork <bork.andrew.ryan@gmail.com>
2022-08-15 14:56:13 -0700

    fdsafds

--- tools/filter_test.cpp
+++ tools/filter_test.cpp
@@ -3,16 +3,36 @@
 #include <filter.h>
 #include <timer.h>
 #include <unistd.h>
+#include <parameters.h>
+#include <socket.h>
 
 timer t;
+filter::filter low_pass = filter::low_pass(24, 5);
+double out = 0;
 void loop() {
-    
+    out += t.dt;
 }
 
 int main() {
     t.start(loop, 1000 / 24);
-    while(1){
-        logger::info("Data Rate: {:f} hz", t.dt);
+    parameters::bind_dbl("out", &out, true);
+
+    sock::socket client(sock::unix, sock::tcp);
+    sock::un_connection unix_connection = client.un_connect(socket_path.c_str());
+
+    if(!unix_connection.valid){
+        // reconnect_node_server(client, unix_connection);
+        logger::crit("No socket server");
+    }
+    while(unix_connection.valid){
+        logger::info("Data Rate: {:f} hz", 1/t.dt);
+
+        std::string sendStr = parameters::get_json_report();
+        int e = unix_connection.send(sendStr.c_str(), sendStr.length());
+        if(e < 0) {
+            reconnect_node_server(client, unix_connection);
+        }
+
         usleep(1000000);
     }
 }
