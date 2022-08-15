4e82f994f1ff304e175958c364aa200b171f39eb
bork <bork.andrew.ryan@gmail.com>
2022-08-15 01:15:42 -0700

    fdsafds

--- tools/timer.cpp
+++ tools/timer.cpp
@@ -6,24 +6,18 @@
 #include <cmath>
 
 int i = 0;
-
-auto then = std::chrono::steady_clock::now();
-auto start = then;
-auto now = std::chrono::steady_clock::now();
+timer t;
 
 void test() {
-    now = std::chrono::steady_clock::now();
-    double dt = std::chrono::duration_cast<std::chrono::nanoseconds> (now - then).count() * 0.000000001;
-    then = now;
     // int s = 14000 * sin(i) + 15000;
     int s = 100;
     usleep(s);
-    logger::info("i: {:10d} sleep: {:10f} s freq: {:10.1f} hz ", i, s/1000000000.0, 1/dt);
+    logger::info("i: {:10d} sleep: {:10f} s freq: {:10.1f} hz ", i, s/1000000000.0, 1/t.dt);
     i++;
 }
 
 int main() {
-    timer t(test, 1000/24);
+    t.start(test, 1000/24);
     usleep(10000000);
     t.interval = 1000/48;
     usleep(10000000);
