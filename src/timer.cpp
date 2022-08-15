e9b6cad25230ba60f773b6336fdaa57901b88f54
bork <bork.andrew.ryan@gmail.com>
2022-08-14 21:29:11 -0700

    fdas

--- src/timer.cpp
+++ src/timer.cpp
@@ -20,16 +20,6 @@ timer::timer(std::function<void(void)> _command, unsigned int _interval_ms) {
     thread = std::thread(bruh, command, running, interval, thread_mutex);
 }
 
-void timer::operator() (){
-    auto chrono_interval = std::chrono::milliseconds(interval);
-    while(running) {
-        std::lock_guard <std::mutex> lock(thread_mutex);
-        auto next = std::chrono::steady_clock::now() + chrono_interval;
-        command();
-        std::this_thread::sleep_until(next);
-    }
-}
-
 timer::~timer() {
     running = false;
 }
