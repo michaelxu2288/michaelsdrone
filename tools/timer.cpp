90af36c9bb7f2cb06cf0ba42d5cb82a6206ede3d
bork <bork.andrew.ryan@gmail.com>
2022-08-14 22:20:24 -0700

    fdsasdfdsa

--- tools/timer.cpp
+++ tools/timer.cpp
@@ -1,10 +1,19 @@
 #include <timer.h>
 #include <logger.h>
 #include <unistd.h>
+#include <chrono>
 
 int i = 0;
+static auto dt;
+
+auto then = std::chrono::steady_clock::now();
+auto start = then;
+auto now = std::chrono::steady_clock::now();
+
 void test() {
-    logger::info("i: {:d}", i);
+    double dt = std::chrono::duration_cast<std::chrono::nanoseconds> (now - then).count() * 0.000000001;
+    int t_since = std::chrono::duration_cast<std::chrono::nanoseconds> (now - start).count();
+    logger::info("i: {:10d} {:10f} ", i, dt);
     i++;
 }
 
