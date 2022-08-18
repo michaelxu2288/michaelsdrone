4f67cbbbd1d042c43e3c4c3ef997a54b5ac44e34
bork <bork.andrew.ryan@gmail.com>
2022-08-18 00:52:12 -0700

    fsdafds

--- src/kalman.cpp
+++ src/kalman.cpp
@@ -1,7 +1,7 @@
 #include <kalman.h>
 
-void kalman1D::predict(double dt) {
-    // x_pred.x = x.x + dt * x.y + 0.5 * dt * dt * x.z; // x = x + v * dt + (1/2) * a * dt
-    // x_pred.y = x.y + x.z * dt; // v = v + a * dt
-    // x_pred.z = x.z; // a = a
-}
+// void kalman1D::predict(double dt) {
+//     // x_pred.x = x.x + dt * x.y + 0.5 * dt * dt * x.z; // x = x + v * dt + (1/2) * a * dt
+//     // x_pred.y = x.y + x.z * dt; // v = v + a * dt
+//     // x_pred.z = x.z; // a = a
+// }
