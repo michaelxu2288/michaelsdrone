8b88fcb803be3c227b7cec2afbede860bf616563
bork <bork.andrew.ryan@gmail.com>
2022-10-06 00:13:00 -0700

    fasdfd

---
+++ tools/gps.cpp
@@ -0,0 +1,15 @@
+#include <logger.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main() {
+    int fd = open("/dev/serial0");
+    
+    char buf[200];
+
+    while(true) {
+        int len = read(fd, buf, 200);
+        logger::info("data: {}", buf);
+    }
+
+}
