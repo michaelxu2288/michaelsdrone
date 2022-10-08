78a43da6b1527a3a6c5c45f529aa5fb7052068c1
bork <bork.andrew.ryan@gmail.com>
2022-10-08 13:35:14 -0700

    fdasfds

--- tools/gps.cpp
+++ tools/gps.cpp
@@ -5,13 +5,13 @@
 #define INTERNAL_BUFFER 1024
 
 int main() {
-    int fd = open("/dev/serial0", O_RDWR | O_BLOCK);
+    int fd = open("/dev/serial0", O_RDWR);
     
     char buf[INTERNAL_BUFFER];
     int len = 0;
 
     while(true) {
-        int chg_len += read(fd, buf + len, INTERNAL_BUFFER - len);
+        int chg_len = read(fd, buf + len, INTERNAL_BUFFER - len);
         if(chg_len != 0) {
             len += chg_len;
             logger::info("length: {}, data: {}", len, buf);
