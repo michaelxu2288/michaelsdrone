f5ef6947fe43ea953555a0502aa337cbc6944c35
bork <bork.andrew.ryan@gmail.com>
2022-10-08 16:44:11 -0700

    fasfds

---
+++ tools/test_util.cpp
@@ -0,0 +1,27 @@
+#include <logger.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <util.h>
+
+#define INTERNAL_BUFFER 1024
+
+int main() {
+    
+    char buf[INTERNAL_BUFFER];
+    char buf2[INTERNAL_BUFFER];
+    int len = 0;
+
+    strcpy(buf, "hello world!");
+    int i = cstr::index_of(buf, "wo");
+    logger::info("{}", i);
+    cstr::shift(buf, i+2, buf2);
+    logger::info("{}", buf2);
+
+    
+    strcpy(buf, "ab !ab !ab !ab !ab !ab !ab !ab !ab");
+    i = cstr::index_of(buf, " !");
+    logger::info("{}", i);
+    cstr::shift(buf, i+2, buf2);
+    logger::info("{}", buf2);
+
+}
