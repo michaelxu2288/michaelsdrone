83900d271210c3df1cd17a64e98fe2523a5b1ec6
bork <bork.andrew.ryan@gmail.com>
2022-10-08 13:30:02 -0700

    fdsa

--- tools/gps.cpp
+++ tools/gps.cpp
@@ -8,15 +8,12 @@ int main() {
     int fd = open("/dev/serial0", O_RDWR | O_BLOCK);
     
     char buf[INTERNAL_BUFFER];
-
+    int len = 0;
 
     while(true) {
-        int len = read(fd, buf, INTERNAL_BUFFER);
-        // while(len --){
-        //     buf[len] = ~buf[len];
-        // }
-        if(len != 0) {
-
+        int chg_len += read(fd, buf + chg_len, INTERNAL_BUFFER);
+        if(chg_len != 0) {
+            len += chg_len;
             logger::info("length: {}, data: {}", len, buf);
         }
     }
