9b8704151989924fab0ae5386294a908a4ad9f3d
bork <bork.andrew.ryan@gmail.com>
2022-10-09 14:04:25 -0700

    fdas

--- src/util.cpp
+++ src/util.cpp
@@ -65,6 +65,7 @@ void cstr::shift(char * in, size_t amount, char * out) {
 
 
 void cstr::split(const char * in, const char * delim, size_t delim_length, std::vector<std::string> out) {
+    printf("%s", in);
     size_t next = cstr::index_of(in, delim, 0);
     size_t last = 0;
     printf("%d %d", last, next);
